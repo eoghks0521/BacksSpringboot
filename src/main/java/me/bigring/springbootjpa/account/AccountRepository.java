5b1bddd8ae4512e8388f159c1bcece5838a65469
daehwan <eoghks0521@naver.com>
2020-06-05 19:45:02 +0900

    #1 Create project & Add jpa

---
+++ src/main/java/me/bigring/springbootjpa/account/AccountRepository.java
@@ -0,0 +1,9 @@
+package me.bigring.springbootjpa.account;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account,Long> {
+	Optional<Account> findByUsername(String username);
+}
